e3ab77e3c7b12f5a162328313cea2f8f43eec1d0
Himanshu kumar <himanshukumar@MacBook-Pro.local>
2020-07-13 01:02:05 +0530

    Updates

---
+++ precache-manifest.82f73ce968742e808721b93eb78f4a5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea74f9d4aa42ba7c412b6c84f53fa8f3",
+    "url": "/github.io/sapient-app/index.html"
+  },
+  {
+    "revision": "ed240a3d02ae72c0de89",
+    "url": "/github.io/sapient-app/static/css/main.7296775f.chunk.css"
+  },
+  {
+    "revision": "c135bd21fd68d5a96e96",
+    "url": "/github.io/sapient-app/static/js/2.28302985.chunk.js"
+  },
+  {
+    "revision": "23ec6c7f0411a6141095a6c739b32980",
+    "url": "/github.io/sapient-app/static/js/2.28302985.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed240a3d02ae72c0de89",
+    "url": "/github.io/sapient-app/static/js/main.4f918486.chunk.js"
+  },
+  {
+    "revision": "b750707ac1f2c9468d8f",
+    "url": "/github.io/sapient-app/static/js/runtime-main.d28d210a.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/github.io/sapient-app/precache-manifest.82f73ce968742e808721b93eb78f4a5c.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/github.io/sapient-app/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
